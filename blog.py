589874c7101889e4a87e69da615e4d2fb289f49d
greg <grisha.novgorodtsev@gmail.com>
2023-04-29 12:41:55 +0300

    Getting test redirect work

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 from Post import Post
 
@@ -11,11 +11,16 @@ posts = [Post("Вася", "Привет", "Привет всем!"),
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
+        if request.form["author"] == "" or request.form["title"] == "" or request.form["message"] == "":
+            return redirect(url_for('badForm'))
         new_post = Post(request.form["author"],
                         request.form["title"],
                         request.form["message"])
         posts.append(new_post)
     return render_template("index.html", posts=posts)
 
+@app.route("/badFrom")
+def badForm():
+    return "Bruh"
 
 app.run(port=5757)
