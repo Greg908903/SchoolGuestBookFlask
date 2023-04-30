2a933c46b5af474887264f81dcbcfb1ce67da1ab
Greg908903 <grisha.novgorodtsev@gmail.com>
2023-04-30 09:43:02 +0300

    Added arguments to bad_form page

--- blog.py
+++ blog.py
@@ -4,6 +4,8 @@ from Post import Post
 
 app = Flask(__name__)
 
+FORM_NAMES = ["author", "title", "message"]
+
 posts = [Post("Вася", "Привет", "Привет всем!"),
          Post("Петя", "Пока", "Всем пока, я пошёл!")]
 
@@ -11,16 +13,24 @@ posts = [Post("Вася", "Привет", "Привет всем!"),
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        if request.form["author"] == "" or request.form["title"] == "" or request.form["message"] == "":
-            return redirect(url_for('badForm'))
+        parameters = []
+        for i in FORM_NAMES:
+            parameters.append(len(request.form[i]) == 0)
+        if True in parameters:
+            return redirect(url_for('bad_form', is_empty_author=parameters[0],
+                                    is_empty_title=parameters[1],
+                                    is_empty_message=parameters[2], count_empty=parameters.count(True)))
         new_post = Post(request.form["author"],
                         request.form["title"],
                         request.form["message"])
         posts.append(new_post)
     return render_template("index.html", posts=posts)
 
-@app.route("/badFrom")
-def badForm():
+
+@app.route("/bad_form")
+def bad_form():
+    count_empty_fields = 0
     return "Bruh"
+
 
 app.run(port=5757)
